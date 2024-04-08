d7d371fa8104136976b65e85c4516ef9802f480a
= <=>
2024-04-08 17:08:17 +0200

    Hello Lua

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <print>
+#include <lua.hpp>
 
 int main()
 {
-  std::println("hello world");
+  auto L = luaL_newstate();
+  luaL_openlibs(L);
+  luaL_dostring(L, "print('Hello Lua', a)");
+  lua_close(L);
+  std::println("Hello C++");
 }

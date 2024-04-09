a49314d2a73b639b9d3553f479ffde5ec80d3d54
= <=>
2024-04-09 16:14:59 +0200

    Prepare for Win32 integration

--- doc.cpp
+++ doc.cpp
@@ -1,5 +1,7 @@
 #include "doc.hpp"
 #include <lua.hpp>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 #include <format>
 
--- doc.hpp
+++ doc.hpp
@@ -2,25 +2,56 @@
 #include <filesystem>
 #include <vector>
 
+struct HWND__;
+typedef HWND__ *HWND;
+
 struct lua_State;
 auto lua_tocode(lua_State *L, int idx, std::output_iterator<char> auto out) noexcept -> decltype(out);
 auto lua_tocode(lua_State *L, int idx) noexcept -> std::string;
 namespace doc
 {
+  using std::string,
+      std::filesystem::path,
+      std::vector;
+
   struct icon
   {
-    std::string title{};
-    std::filesystem::path icon_path{};
+    string title{};
+    path icon_path{};
 
+    HWND hwnd{};
+
+    /// @brief Copy the value at idx on the lua stack into an icon object
+    /// @param L
+    /// @param idx
+    /// @param output
+    /// @return
     friend bool pull(lua_State *L, int idx, icon &output);
+
+    /// @brief Copy an icon object top the top of the lua stack
+    /// @param L
+    /// @param input
+    /// @return
     friend bool push(lua_State *L, icon const &input);
   };
 
   struct doc
   {
-    std::vector<icon> icons{};
+    vector<icon> icons{};
 
+    HWND hwnd{};
+
+    /// @brief Copy the value at idx on the lua stack into a doc object
+    /// @param L
+    /// @param idx
+    /// @param output
+    /// @return
     friend bool pull(lua_State *L, int idx, doc &output);
+
+    /// @brief Copy a doc object top the top of the lua stack
+    /// @param L
+    /// @param input
+    /// @return
     friend bool push(lua_State *L, doc const &input);
   };
 } // namespace doc
